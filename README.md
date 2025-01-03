b695b74b0b83157617fde58161415a5b28893dbb
Chris Stinson <cistinson@lcmail.lcsc.edu>
2025-01-02 22:08:20 -0800

    Update title

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ShoppingListApp
+# MyList
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.4.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
-import {ChangeDetectionStrategy} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import { CardFancyExample } from './shared/card-fancy-example/card-fancy-example.component';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatCardModule, MatButtonModule, CardFancyExample, MatSlideToggleModule],
+  imports: [FormsModule, MatInputModule, MatButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'shopping-list-app';
+  title = 'MyList';
 }
