605ab1f28451f50abd6ae152c96caa0aa7bfd621
Chris Stinson <cistinson@lcmail.lcsc.edu>
2025-01-04 23:05:49 -0800

    Base nav set up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-
-
-  bootstrapApplication(AppComponent, {
-    providers: [
-      BrowserAnimationsModule,
-      provideRouter(routes)
-    ],
-  }).catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimations(),  // Enables animations for Angular Material
+    provideHttpClient(),  // Enables HTTP client
+    provideRouter(routes) // Adds routing support
+  ]
+}).catch((err) => console.error(err));
