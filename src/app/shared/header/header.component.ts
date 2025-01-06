ed3a1b2ccd279c28696355abddc0b9dfd78dfc9d
Chris Stinson <cistinson@lcmail.lcsc.edu>
2025-01-05 21:53:30 -0800

    Nav skeleton ready

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,16 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatSidenav,
+    AsyncPipe,
+    NgIf
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @ViewChild('drawer') drawer!: MatSidenav;
+  private readonly breakpointObserver: BreakpointObserver;
+  isHandset$: Observable<boolean>;
 
+  constructor(breakpointObserver: BreakpointObserver) {
+    this.breakpointObserver = breakpointObserver;
+    this.isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset)
+      .pipe(
+        map(result => result.matches),
+        shareReplay()
+      );
+  }
 }
