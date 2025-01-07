b2c34eeab74b1c6c827ec04373e126c96453c605
Chris Stinson <cistinson@lcmail.lcsc.edu>
2025-01-06 20:56:59 -0800

    Add service and test data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { ListDataDefaultService } from '../../shared/services/list-data-default/list-data-default.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, MatInputModule, MatButtonModule],
+  imports: [FormsModule, MatInputModule, MatButtonModule, NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  testItemsList: string[] = [];
+  listDataDefaultService: ListDataDefaultService = inject(ListDataDefaultService);
+
+  constructor(){
+    this.testItemsList = this.listDataDefaultService.getItems();
+  }
 
 }
---
+++ src/app/shared/services/list-data-default/list-data-default.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { ShoppingListModel } from '../../models/shopping-list.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListDataDefaultService {
+
+  //Values for forms
+  public items: string[] = [
+    "Apples",
+    "Bananas",
+    "Carrots",
+    "Milk",
+    "Eggs",
+    "Bread",
+    "Cheese",
+    "Chicken",
+    "Rice",
+    "Pasta",
+    "Tomatoes",
+    "Potatoes",
+    "Onions",
+    "Lettuce",
+    "Cereal",
+    "Yogurt",
+    "Butter",
+    "Coffee",
+    "Tea",
+    "Sugar",
+    "Salt",
+    "Pepper",
+    "Flour",
+    "Olive Oil",
+    "Canned Beans",
+    "Canned Tomatoes",
+    "Frozen Peas",
+    "Frozen Pizza",
+    "Ice Cream",
+    "Snack Bars",
+    "Chips",
+    "Cookies",
+    "Juice",
+    "Soda",
+    "Toilet Paper",
+    "Paper Towels",
+    "Dish Soap",
+    "Laundry Detergent"
+  ]
+
+
+  constructor() { }
+
+
+  getItems(): string[]{
+    return this.items;
+  }
+
+
+}
