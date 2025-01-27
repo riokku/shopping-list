e400d1bb7d89106ef6dd032de529a7644581c070
Chris Stinson <cistinson@lcmail.lcsc.edu>
2025-01-26 22:17:48 -0800

    Add new item form basics

--- src/app/pages/create-list/create-list.component.ts
+++ src/app/pages/create-list/create-list.component.ts
@@ -4,10 +4,13 @@ import { StoreModel } from '../../shared/models/store.model';
 import { ListDataDefaultService } from '../../shared/services/list-data-default/list-data-default.service';
 import { StoreItemModel } from '../../shared/models/store-item.model';
 import { Observable } from 'rxjs';
-import { FormsModule, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, FormControl, ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { CommonModule } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { ShoppingListItemModel } from '../../shared/models/shopping-list-item.model';
 
 @Component({
   selector: 'app-create-list',
@@ -17,6 +20,8 @@ import { CommonModule } from '@angular/common';
     ReactiveFormsModule,
     MatInputModule,
     MatAutocompleteModule,
+    MatIcon,
+    MatButtonModule,
     CommonModule
   ],
   templateUrl: './create-list.component.html',
@@ -28,30 +33,42 @@ export class CreateListComponent {
   public selectedStoreItems: StoreItemModel[] | undefined;
   public listDataDefaultService: ListDataDefaultService = inject(ListDataDefaultService);
 
+  public showAddItemForm: boolean = false;
+
+  shoppingForm: FormGroup;
+
   storeItemsControl = new FormControl('');
   filteredOptions!: Observable<StoreModel[]>;
   selectedItems: any[] = []; // Array to store selected items
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder) {
     this.route.params.subscribe((params: Params) => {
       this.storeGUID = params['guid'];
       this.selectedStore = this.listDataDefaultService.getSpecificStore(this.storeGUID);
     });
 
-
-    this.storeItemsControl.valueChanges.subscribe((selectedItem) => {
-      if (selectedItem) {
-        this.addItem(selectedItem);
-      }
-    });
+    this.shoppingForm = this.formBuilder.group({
+      item: '',
+      quantity: '',
+      notes: ''
+    })
   }
 
-  addItem(item: any): void {
+  addItem(item: ShoppingListItemModel): void {
     // Check if the item is already added
     const isAlreadyAdded = this.selectedItems.some((i) => i.name === item.name);
     if (!isAlreadyAdded) {
       this.selectedItems.push(item);
     }
+  }
+
+  onSubmit(){
+    const isAlreadyAdded = this.selectedItems.some((i) => i.item.name === this.shoppingForm.value.item.name);
+    if (!isAlreadyAdded) {
+      this.selectedItems.push(this.shoppingForm.value);
+    }
+    console.log(this.shoppingForm.value.name);
+
   }
 
   displaySelectedItem(item: StoreItemModel): string {
