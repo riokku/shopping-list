aa80bdfd14e5f628ae1d4aed6d5b2b38abeba496
Chris Stinson <cistinson@lcmail.lcsc.edu>
2025-02-27 21:23:21 -0800

    Add initial auth functions

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { SupabaseClient, createClient } from '@supabase/supabase-js';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
-import { environment } from '../../../environments/environment';
+import { SupabaseService } from '../../shared/services/supa/supa.service';
 
 @Component({
   selector: 'app-login',
@@ -17,33 +18,56 @@ import { environment } from '../../../environments/environment';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatCardModule,
+    MatIconModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   loginForm: FormGroup;
-  private supabase: SupabaseClient;
+  signUpForm: FormGroup;
+  public loggedIn: boolean = false;
 
-  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {
-    this.supabase = createClient(environment.supabaseUrl, environment.supabaseAnonKey);
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar, private supaService: SupabaseService) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
+    this.signUpForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  switchMode(){
+    this.loggedIn = !this.loggedIn;
+  }
+
+  async signUp() {
+    const email = this.signUpForm.value.email;
+    const password = this.signUpForm.value.password;
+
+    this.supaService.signUp(email, password).then((res) => {
+      console.log(res);
+    }).catch((err) => {
+      console.log(err);
+
+    })
   }
 
   async login() {
     if (this.loginForm.invalid) return;
     const { email, password } = this.loginForm.value;
 
-    const { error } = await this.supabase.auth.signInWithPassword({ email, password });
+    // const { error } = await this.supaService.signIn(email, password);
 
-    if (error) {
-      this.snackBar.open(error.message, 'Close', { duration: 3000 });
-    } else {
-      this.snackBar.open('Login successful!', 'Close', { duration: 3000 });
-    }
+    // if (error) {
+    //   this.snackBar.open(error.message, 'Close', { duration: 3000 });
+    // } else {
+    //   this.snackBar.open('Login successful!', 'Close', { duration: 3000 });
+    // }
   }
 }
+
